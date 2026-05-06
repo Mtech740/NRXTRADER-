4ac7cf570994db439865832c8c1a2d793e146084
Mtech740 <mumbasampa3@gmail.com>
2026-05-06 22:33:09 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,6 @@
 const { Pool } = require('pg');
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false } // needed for Railway / Supabase
+    ssl: false   // disable SSL for internal Render communication
 });
-
 module.exports = pool;
